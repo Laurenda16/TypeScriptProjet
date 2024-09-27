ed10af84436040e0cbe5b6e6275d000efa74c405
Laurenda Edorh <laurenda98@gmail.com>
2024-09-27 23:38:17 +0200

    suppression de chaque container

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,36 @@
 const itemsContainer = document.querySelectorAll('.items-container') as NodeListOf<HTMLDivElement>
+let actualContainer:HTMLDivElement;
+let actualBtn :HTMLButtonElement;
+let actualUL:HTMLUListElement;
+let actualForm:HTMLFormElement;
+let actualTextInput:HTMLInputElement;
+let actualValidation:HTMLSpanElement;
+
+
+
+
 
 
 function addContainerListners(currentContainer: HTMLDivElement)
 {
     const currentContainerDeletionBtn = currentContainer.querySelector('.delete-container-btn') as HTMLButtonElement;
-deleteBtnListeners(currentContainerDeletionBtn);
+    const currentAddItemBtn = currentContainer.querySelector('.add-item-btn') as HTMLButtonElement
+    deleteBtnListeners(currentContainerDeletionBtn);
+    addItemBtnListeners(currentAddItemBtn);
 }
 itemsContainer.forEach((container: HTMLDivElement)=>
 {
     addContainerListners(container);
 })
+
+
+//function2
+function addItemBtnListeners(btn:HTMLButtonElement)
+
+{
+btn.addEventListener("click", handleAddItem)
+}
+
 
 function deleteBtnListeners(btn:HTMLButtonElement)
 
@@ -24,5 +45,30 @@ const btnsArray = [...document.querySelectorAll('.delete-container-btn')] as HTM
 const containers = [...document.querySelectorAll('.items-container')] as HTMLDivElement[];
 
 containers[btnsArray.indexOf(btn)].remove();
+
+}
+
+function handleAddItem(e:MouseEvent)
+{
+    const btn = e.target as HTMLButtonElement;
+    setContainerItems(btn);
+    toggleForm(actualBtn,actualForm,true);
+
+}
+function toggleForm(btn:HTMLButtonElement, form:HTMLFormElement, action:boolean)
+{
+
+}
+///fonction setConatainer
+
+function setContainerItems(btn:HTMLButtonElement)
+{
+  actualBtn =btn;
+ actualContainer= btn.parentElement as HTMLDivElement;
+
+ actualUL= actualContainer.querySelector('ul') as HTMLUListElement;
+actualForm= actualContainer.querySelector('form') as HTMLFormElement;
+ actualTextInput= actualContainer.querySelector('input') as HTMLInputElement;
+ actualValidation = actualContainer.querySelector('.validation-msg') as HTMLSpanElement;
 
 }
